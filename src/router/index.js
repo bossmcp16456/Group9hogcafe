86698f5f883d8021d46ab1a808e79036bc8e356f
Supanat Litchaweerat <6331305038@lamduan.mfu.ac.th>
2021-11-06 23:32:58 +0700

    sp commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,12 +12,15 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Lobby',
+    name: 'Lobby',
+    
+    component: () => import('../views/Lobby.vue')
+  },
+  {
+    path: '/farm',
+    name: 'Farm',
+    component: () => import('../views/Farm.vue')
   }
 ]
 
